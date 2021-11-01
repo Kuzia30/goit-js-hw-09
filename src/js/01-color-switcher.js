297900d2000eb210c967437b147e07c5112e5c3d
Kuzia30 <maxzimka37@gmail.com>
2021-11-01 15:23:12 +0200

    finish task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startEl = document.querySelector('button[data-start]');
+const stopEl = document.querySelector('button[data-stop]');
+const bodyEl = startEl.parentNode;
 
+startEl.addEventListener('click', onStartChangeColor)
+let changeColorId = 0;
+
+function onStartChangeColor() {
+    if (!changeColorId) {
+        changeColorId = setInterval(onChangeColor, 1000);
+ }   
+};
+
+stopEl.addEventListener('click', onStopChangeColor);
+
+function onStopChangeColor() {
+    if (changeColorId) {
+        clearInterval(changeColorId);
+        changeColorId = 0;
+ }   
+}
+
+function onChangeColor() {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
