93ba52c25a105e81e7d5e191883bb4647af8d10e
Kuzia30 <maxzimka37@gmail.com>
2021-11-01 21:58:26 +0200

    add onclose

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,4 +1,34 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-flatpickr("#datetime-picker", {});
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+};
+const calendar = new flatpickr("#datetime-picker", options);
+
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+};
+const date = new Date();
+let totalTime = 0;
+
+refs.startBtn.setAttribute('disabled', true);
+
+calendar.config.onClose.push(function (selectedDates) {
+  totalTime = selectedDates[0].getTime() - date.getTime();
+  console.log(totalTime);
+ } )
+
+
+
+
+
+
+
+
+
+
+
