89090219e57ea6fd2da71846e6e6ac2f4632cf12
Kuzia30 <maxzimka37@gmail.com>
2021-11-02 21:24:44 +0200

    finish task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const options = {
   enableTime: true,
@@ -11,33 +12,41 @@ const calendar = new flatpickr("#datetime-picker", options);
 
 
 const refs = {
+  input: document.querySelector('#datetime-picker'),
   startBtn: document.querySelector('button[data-start]'),
   days: document.querySelector('.value[data-days]'),
   hours: document.querySelector('.value[data-hours]'),
   minutes: document.querySelector('.value[data-minutes]'),
   seconds: document.querySelector('.value[data-seconds]')
 };
-
+// const timeLeftObf = {};
 let selectedDate = 0;
 let timeLeft = 0;
+let id = null;
 
 refs.startBtn.setAttribute('disabled', true);
-refs.startBtn.addEventListener('click', onStartTime);
+refs.startBtn.addEventListener('click', onStart);
 
 
 calendar.config.onClose.push(function (selectedDates) {
   if (selectedDates[0].getTime() <= Date.now()) {
     refs.startBtn.setAttribute('disabled', true);
-    return window.alert("Please choose a date in the future");
+    return Notify.failure("Please choose a date in the future");
   }
   refs.startBtn.removeAttribute('disabled');
   selectedDate = selectedDates[0].getTime();
 });
 
-function subtractTime() {
-  timeLeft = selectedDate -  Date.now();
-  console.log( timeLeft);
-}
+function onTimer() {
+  timeLeft = selectedDate - Date.now();
+  stopTimeOut(timeLeft);
+  const convertObj = convertMs(timeLeft);
+  if (timeLeft < 0) {
+  return
+  }
+  showDate(convertObj);
+};
+
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -56,18 +65,40 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
-}
+};
 
-function onStartTime() {
-  subtractTime();
-  const timeLeftObf = convertMs(timeLeft);
-  refs.days.textContent = timeLeftObf.days;
-  refs.hours.textContent = timeLeftObf.hours;
-  refs.minutes.textContent = timeLeftObf.minutes;
-  refs.seconds.textContent = timeLeftObf.seconds;
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+};
+
+function stopTimeOut(sec) {
+  if (sec < 1000) {
+    Notify.success('Time is out');
+    clearTimeout(id);
+    refs.input.removeAttribute('disabled');
+    return
+  };
+  addTimeOut(); 
+};
+
+
+function showDate(time) {
   
-}
+  refs.days.textContent = addLeadingZero(time.days);
+  refs.hours.textContent = addLeadingZero(time.hours);
+  refs.minutes.textContent = addLeadingZero(time.minutes);
+  refs.seconds.textContent = addLeadingZero(time.seconds);
+};
 
+function addTimeOut() {
+  id = setTimeout(onTimer, 1000);
+};
+
+function onStart() {
+  addTimeOut();
+  refs.input.setAttribute('disabled', true);
+  refs.startBtn.setAttribute('disabled', true);
+};
 
 
 
